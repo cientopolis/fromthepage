fd2e008a8ba8e9d7ab1ffd612a75aba6b964ec23
CEBracco <development@transcriptor-christian.home>
2020-07-04 20:15:16 -0300

    generic ontology queries

--- app/controllers/api/ontology_controller.rb
+++ app/controllers/api/ontology_controller.rb
@@ -3,12 +3,7 @@ class Api::OntologyController < Api::ApiController
     before_action :set_ontology, only: [:update, :destroy]
 
     def create
-        @ontology = Ontology.new
-        @ontology.name = params[:ontology][:name]
-        @ontology.description = params[:ontology][:description]
-        @ontology.url = params[:ontology][:url]
-        @ontology.domainkey = params[:ontology][:domainkey]
-        @ontology.prefix = params[:ontology][:prefix]
+        @ontology = Ontology.new(ontology_params)
         if @ontology.save
         render_serialized ResponseWS.ok("api.ontology.create.success",@ontology,alert)
         else
@@ -17,7 +12,7 @@ class Api::OntologyController < Api::ApiController
     end
 
     def update
-        @ontology.update_attributes(params[:ontology].permit(:name,:description,:url,:domainkey,:prefix))
+        @ontology.update_attributes(ontology_params)
         render_serialized ResponseWS.ok("api.ontology.update.success",@ontology)
     end
 
@@ -32,8 +27,13 @@ class Api::OntologyController < Api::ApiController
         response_serialized_object @ontologies
     end
 
-    def set_ontology
-        @ontology = Ontology.find(params[:id])
-        raise ActiveRecord::RecordNotFound unless @ontology
-    end
+    private
+        def set_ontology
+            @ontology = Ontology.find(params[:id])
+            raise ActiveRecord::RecordNotFound unless @ontology
+        end
+
+        def ontology_params
+            params[:ontology].permit(:name,:description,:url,:domainkey,:rangekey,:prefix,:literal_type)
+        end
 end
