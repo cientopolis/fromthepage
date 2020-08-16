5d7ca25297358953e543fb946f9cb322c5e26132
CEBracco <development@transcriptor-christian.home>
2020-08-16 00:35:08 -0300

    fix function roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,3 @@
 class Role < ActiveRecord::Base
-    has_and_belongs_to_many  :functionrole
+    has_and_belongs_to_many  :functionrole, :class_name => 'Functionrole', :join_table => :functionroles_roles
 end
